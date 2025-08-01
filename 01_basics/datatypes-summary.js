958a419c8ea95a20896e09f531cc7122071280d5
Soumalya20 <soumalya58342@gmail.com>
2025-08-01 14:06:51 +0530

    Stack and heap memory update
    Added Stack memory and heap memory brief to the datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -44,3 +44,37 @@ console.log(typeof myFunction); // "function"
 console.log(typeof bigIntValue); // "bigint"
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3 (STUDY MATERIAL)
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+// Stack memory (Primitive types), Heap memory (Reference types)
+
+
+// Primitive types are stored in stack memory, while reference types are stored in heap memory.
+
+// Stack memory is faster and has a fixed size, while heap memory is slower and can grow dynamically.
+// When you create a primitive type, it is stored directly in the stack.
+// When you create a reference type, a reference (or pointer) to the object is stored in the stack, while the actual object is stored in the heap.
+
+let myYoutubeName ="X4mmy";
+let anotherName = myYoutubeName; // Copying value (Primitive type)
+anotherName = "Soumalya"; // Changing anotherName does not affect myYoutubeName
+
+// In contrast, if we had used a reference type, changing the new variable would affect the original variable.
+
+console.log(myYoutubeName); // "X4mmy"
+console.log(anotherName); // "Soumalya"
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@upi",
+}
+
+let userTwo = userOne; // Copying reference (Reference type)
+userTwo.email = "soumalya@google.com"; // Changing userTwo affects userOne
+
+console.log(userOne.email); // 
+console.log(userTwo.email); // Both will show "soumalya@google.com"
